153f21fefc4b09989d88426df062c5e5c801d3c3
Francis <fmdp93@gmail.com>
2024-04-22 19:46:15 +0800

    update ProductFactory definition

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,9 +2,11 @@
 
 namespace Database\Factories;
 
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
 // use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Config;
 
 class ProductFactory extends Factory
 {
@@ -14,19 +16,29 @@ class ProductFactory extends Factory
      * @return array
      */
 
-    protected $model = \App\Models\Product::class;
+    protected $model = Product::class;
 
     public function definition()
     {
+        /**
+         * @var int $category_id
+         */
+        $category_id = Category::where('id', rand(1, SEED_COUNT))->first()->id;
+        $this->faker->locale('en_US');
+        $price = rand(100, 1000) + rand(1, 100) * .1;
+        $markup = Config::get('app.markup_price');
         return [
             'item_code' => $this->faker->ean13(),
-            'category_id' => rand(1,10),
+            'category_id' => $category_id,
             'stock' => 100,
-            'price' => rand(100, 1000) + rand(1, 100) * .1,
-            'name' => $this->faker->name(),
+            'price' => $price,
+            'name' => $this->faker->productName($category_id),
             'unit' => 'piece',
-            'description' => $this->faker->paragraph(1),
+            'description' => $this->faker->realText(),
             'expiration_date' => $this->faker->date(),
+            'supplier_id' => rand(1,10),
+            'base_price' => $price / (1 + $markup/100),
+            'markup' => $markup,
         ];
     }
 }

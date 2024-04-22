b322a807cd40048199862bf385790a78171c2071
Francis <fmdp93@gmail.com>
2024-04-22 19:48:44 +0800

    update ProductCategorySeeder run

--- database/seeders/ProductCategorySeeder.php
+++ database/seeders/ProductCategorySeeder.php
@@ -15,6 +15,31 @@ class ProductCategorySeeder extends Seeder
      */
     public function run()
     {
-        Category::factory()->count(10)->create();
+        $categories = [
+            'Rice',
+            'Canned Goods',
+            'Instant Noodles',
+            'Beverages (Soft Drinks, Juices, etc.)',
+            'Cooking Oil',
+            'Condiments (Soy Sauce, Vinegar, etc.)',
+            'Snacks (Chips, Biscuits, etc.)',
+            'Frozen Foods',
+            'Fresh Produce (Fruits and Vegetables)',
+            'Meat and Poultry',
+            'Fish and Seafood',
+            'Dairy Products (Milk, Cheese, Yogurt, etc.)',
+            'Bakery Products (Bread, Pastries, etc.)',
+            'Cereals and Breakfast Items',
+            'Instant Coffee and Tea',
+            'Canned Meat (Corned Beef, Luncheon Meat, etc.)',
+            'Pasta and Noodles (Spaghetti, Macaroni, etc.)',
+            'Canned Fruits and Vegetables',
+            'Sauces and Dressings',
+            'Baking Supplies (Flour, Sugar, etc.)',
+        ];
+
+        foreach ($categories as $category){
+            Category::insert(['name' => $category]);
+        }
     }
 }

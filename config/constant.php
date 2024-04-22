c4fb25fb635d5829f2ae409137b3e431e00d03fb
Francis <fmdp93@gmail.com>
2024-04-22 19:42:18 +0800

    Added constant seed_count

--- config/constant.php
+++ config/constant.php
@@ -8,6 +8,7 @@ defined('TERM_30_DAYS') || define('TERM_30_DAYS', 3);
 defined('PATTERN_ID') || define('PATTERN_ID', '[0-9]+');
 defined('STATUS_ORDER_RECEIVED') || define('STATUS_ORDER_RECEIVED', 2);
 defined('CONFIG_SERIAL_NUMBER') || define('CONFIG_SERIAL_NUMBER', 4);
+defined('SEED_COUNT') || define('SEED_COUNT', 20);
 return [
   'per_page' => 10,
   'autocomplete_suggestion_count' => 5,
